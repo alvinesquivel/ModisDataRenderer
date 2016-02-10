b1754a98097b52478bd69a91ca8d558d4aeabf5a
Mark Steven Luber <markhluber7@gmail.com>
2016-02-11 04:22:46 +0800

    attributes page

--- modisApp.py
+++ modisApp.py
@@ -23,6 +23,7 @@ class modisApp(tk.Tk):
 		container.pack(side="top", fill="both", expand = True)
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
+
 #MENU BAR
 		menubar = tk.Menu(container)
 
@@ -31,7 +32,7 @@ class modisApp(tk.Tk):
 		menubar.add_cascade(label='File', menu=filemenu)
 
 		helpmenu = tk.Menu(menubar, tearoff=0)
-		helpmenu.add_command(label='About',command= lambda: mbox.showerror('About','Nothing yet'))
+		helpmenu.add_command(label='About',command= lambda: mbox.showinfo('About','Nothing yet'))
 		menubar.add_cascade(label='Help',menu=helpmenu)
 
 		tk.Tk.config(self, menu=menubar)
@@ -44,7 +45,7 @@ class modisApp(tk.Tk):
 
 			self.frames[F] = frame
 
-			frame.grid(row=0, column=0, sticky="nsew")
+			frame.grid(row=0, column=0, sticky="NSEW")
 
 		self.show_frame(SplashPage)	#First window to show - Splash screen
 
@@ -95,90 +96,131 @@ class InputPage(tk.Frame):
 			entrybox2.delete(0,END)
 			entrybox3.delete(0,END)
 
+#FRAME LAYOUT
+
+		self.grid_columnconfigure(1, weight = 1)
+		self.grid_rowconfigure(1, weight = 1)
+
+		topframe = Frame(self)
+		topframe.grid(column = 1)
+		innerframe = Frame(self)
+		innerframe.grid(column = 1, sticky = N+S+E+W, padx = 15)
+		bottomframe = Frame(self)
+		bottomframe.grid(column = 1, sticky = E, padx = 15)
+
+		innerframe.grid_columnconfigure(2, weight = 1)
+		innerframe.grid_rowconfigure(2, weight = 1)
+		innerframe.grid_rowconfigure(4, weight = 1)
+		innerframe.grid_rowconfigure(6, weight = 1)
+
 #LABELS
-		label = ttk.Label(self, text="Input", font=L_FONT)
-		label.grid(row=0,column=0, pady = 10)
+		label = ttk.Label(topframe, text="Input", font=L_FONT)
+		label.pack(pady = 10)
 
-		label1 = ttk.Label(self, text="MOD06 Cloud Properties", font=M_FONT)
-		label1.grid(row=2,column=0,sticky=W, padx = 5, pady = 5)
+		label1 = ttk.Label(innerframe, text="MOD06 Cloud Properties", font=M_FONT)
+		label1.grid(row=2,column=0, padx = 10, pady = 10)
 
-		label2 = ttk.Label(self, text="MOD35 Cloud Mask", font=M_FONT)
-		label2.grid(row=4,column=0,sticky=W, padx = 5, pady = 5)
+		label2 = ttk.Label(innerframe, text="MOD35 Cloud Mask", font=M_FONT)
+		label2.grid(row=4,column=0,padx = 10, pady = 10)
 
-		label3 = ttk.Label(self, text="MOD03 Geolocation", font=M_FONT)
-		label3.grid(row=6,column=0,sticky=W, padx = 5, pady = 5)
+		label3 = ttk.Label(innerframe, text="MOD03 Geolocation", font=M_FONT)
+		label3.grid(row=6,column=0,padx = 10, pady = 10)
 
-
-							#	.grid() layout manager ignores empty rows and columns
-							#	left an empty row between the 3 labels for easier adjusting in the future
 #TEXT BOXES
-		entrybox1 = ttk.Entry(self)
-		entrybox1.grid(row=2,column=2,padx = 5, pady = 5)
+		entrybox1 = ttk.Entry(innerframe)
+		entrybox1.grid(row=2,column=2, padx = 10, pady = 10, sticky = EW)
 
-		entrybox2 = ttk.Entry(self)
-		entrybox2.grid(row=4,column=2, padx = 5, pady = 5)
+		entrybox2 = ttk.Entry(innerframe)
+		entrybox2.grid(row=4,column=2, padx = 10, pady = 10, sticky = EW)
 
-		entrybox3 = ttk.Entry(self)
-		entrybox3.grid(row=6,column=2, padx = 5, pady = 5)
+		entrybox3 = ttk.Entry(innerframe)
+		entrybox3.grid(row=6,column=2, padx = 10, pady = 10, sticky = EW)
 
 #BUTTONS
 
-		button1 = ttk.Button(self, text="Browse", command=OpenHDF_cp)
-		button1.grid(row=2,column=4, padx = 5, pady = 5)
+		button1 = ttk.Button(innerframe, text="Browse", command=OpenHDF_cp)
+		button1.grid(row=2,column=4, padx = 10, pady = 10)
 
-		button2 = ttk.Button(self, text="Browse", command=OpenHDF_cm)
-		button2.grid(row=4,column=4, padx = 5, pady = 5)
+		button2 = ttk.Button(innerframe, text="Browse", command=OpenHDF_cm)
+		button2.grid(row=4,column=4, padx = 10, pady = 10)
 
-		button3 = ttk.Button(self, text="Browse", command=OpenHDF_geo)
-		button3.grid(row=6,column=4, padx = 5, pady = 5)
+		button3 = ttk.Button(innerframe, text="Browse", command=OpenHDF_geo)
+		button3.grid(row=6,column=4, padx = 10, pady = 10)
 
-		checkButton = ttk.Button(self, text="Check Attributes", #moves to page one at the moment
-							command=lambda: controller.show_frame(AttributesPage)) #lambda overrides run at creation behavior, see examples online
-		checkButton.grid(row=8,column=2,sticky=E, padx = 5, pady = 5)
+		button4 = ttk.Button(bottomframe, text="Clear", command=Clear)
+		button4.grid(column = 1, row = 0,padx = 10, pady = 10)
 
-		nextButton = ttk.Button(self, text="Next",
+		button5 = ttk.Button(bottomframe, text="Check Attributes",
+							command=lambda: controller.show_frame(AttributesPage))
+		button5.grid(column = 2, row = 0,padx = 10, pady = 10)
+
+		button6 = ttk.Button(bottomframe, text="Next",
 							command=lambda: controller.show_frame(RenderPage))
-		nextButton.grid(row=8,column=4, padx = 5, pady = 5)
+		button6.grid(column = 3, row = 0, padx = 10, pady = 10)
 
-		clear = ttk.Button(self, text="Clear", command=Clear)
-		clear.grid(row=8, column=0, padx = 5, pady = 5)
+
 
 
 class AttributesPage(tk.Frame):
 
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label1 = ttk.Label(self, text="Attributes", font=L_FONT)
-		label1.grid(row = 0, column = 2, padx = 5, pady = 5)
-#LISTBOX SELECTOR (MOD)
 
-		listbox1 = Listbox(self, font = L_FONT, width = 8, height = 8)
-		listbox1.insert(END, 'MOD 06')
-		listbox1.insert(END, 'MOD 35')
-		listbox1.insert(END, 'MOD 03')
-		listbox1.grid(row = 2,column = 0,padx = 5,pady=5)
+# DEFINING METHODS
 
-#LISTBOX SELECTOR (SCIENTIFIC DATA SETS)
+# FRAME LAYOUTS
 
-		listbox2 = Listbox(self, font = M_FONT, width = 10, height = 8)
+		self.grid_columnconfigure(1, weight = 1)
+		self.grid_rowconfigure(1, weight = 1)
 
-		#Create event to populate list when MOD is selected
+		frame0 = Frame(self)
+		frame0.grid(columnspan = 2)
+		frame1 = Frame(self)
+		frame1.grid(column = 0, sticky = N)
+		frame2 = Frame(self)
+		frame2.grid(row = 1, column = 1, sticky = N+W)
+		frame3 = Frame(self)
+		frame3.grid(columnspan = 2, sticky = E)
 
-		listbox2.grid(row = 2,column = 2, padx = 5,pady = 5)
+		frame2.grid_columnconfigure(1, weight = 1)
 
-#TEXT AREA
+# LABELS
 
-		textarea = Text(self, width = 20, height = 10)
-		textarea.grid(row = 2,column = 6, padx = 5,pady = 5)
+		label0 = ttk.Label(frame0, text = "Attributes", font = L_FONT).pack(padx = 10, pady = 10)
+		label1 = ttk.Label(frame1, text = "Products", font = M_FONT).grid(padx = 10, pady = 10)
+		label2 = ttk.Label(frame1, text = "Scientific Data Sets", font = M_FONT).grid(row = 0, column = 1, padx = 10, pady = 10)
+		label3 = ttk.Label(frame2, text = "Metadata", font = M_FONT).pack(padx = 10, pady = 10)
 
-#BUTTONS
-		button1 = ttk.Button(self, text="Back",
-							command=lambda: controller.show_frame(InputPage))
-		button1.grid(row = 10, column = 2, padx = 5, pady = 5)
+# BUTTONS
 
-		button2 = ttk.Button(self, text="Next",
-							command=lambda: controller.show_frame(RenderPage))
-		button2.grid(row = 10, column = 6, padx = 5, pady = 5)
+		button0 = ttk.Button(frame3, text = "Render").pack(side = RIGHT, padx = 10, pady = 10)
+		button1 = ttk.Button(frame3, text = "Show Values").pack(side = RIGHT, padx = 10, pady = 10)
+		button2 = ttk.Button(frame3, text = "Back").pack(side = RIGHT, padx = 10, pady = 10)
+
+# LISTBOXES
+
+		listbox0 = tk.Listbox(frame1, font = M_FONT)
+		listbox0.grid(padx = 10, pady = 10)
+		listbox0.insert(1, "Cloud Mask")
+		listbox0.insert(2, "Cloud Products")
+		listbox0.insert(3, "Geolocation")
+
+		scrollbar = ttk.Scrollbar(frame1, orient = "vertical")
+
+		listbox1 = tk.Listbox(frame1, font = M_FONT, yscrollcommand = scrollbar.set)
+		listbox1.grid(row = 1, column = 1, padx = 10, pady = 10)
+		scrollbar.config(command=listbox1.yview)
+
+		for i in range (1,25):
+			listbox1.insert(i, i)
+
+		scrollbar.grid(row = 1, column = 2, sticky = N+S, pady = 10)
+
+
+# TEXT BOX
+
+		textbox0 = tk.Text(frame2)
+		textbox0.pack(padx = 10, pady = 10)
 
 
 class RenderPage(tk.Frame):
