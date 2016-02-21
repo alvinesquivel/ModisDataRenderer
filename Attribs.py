8bee45d6930fca6cc6dacd345a3e2fc0c972df6b
Alvin Esquivel <ezkie96@gmail.com>
2016-02-21 10:10:00 +0800

    2/21/2016

--- Attribs.py
+++ Attribs.py
@@ -3,24 +3,33 @@ from pyhdf.SD import SD, SDC, SDS
 import sys
 
 
-_file = 'C:\Users\AKO NA LNG\Desktop\Esquivel Files\School Files 2\Special Problem\MODIS\MOD35\MOD35_L2.A2015220.0215.006.2015220134621.hdf'
-mod35 = SD(_file, SDC.READ)
+_file = 'C:\Users\AKO NA LNG\Desktop\Esquivel Files\School Files 2\Special Problem\MODIS\MOD06\MOD06_L2.A2015348.0215.006.2015348152515.hdf'
+mod06 = SD(_file, SDC.READ)
 
 
 #List SDS
 
-ds = mod35.datasets()
+ds = mod06.datasets()
+
 ds_lst = ds.keys()
 
 for i in range(len(ds_lst)):
 	print ds_lst[i]
-	print "\n"
 
-sds_name = "Cloud_Mask"
-sds = mod35.select(sds_name)
+
+#Metadata for Cloud Optical Thickness
+sds_name = "Cloud_Optical_Thickness"
+sds = mod06.select(sds_name)
 
 data = sds.attributes(full=1)
-print type(data['description'])
+data_keys_lst = data.keys()
+print "\n"
+for key in data_keys_lst:
+	print key + ":"
+	print data[key]
+	print "\n"
 
 
-mod35.end()
+
+
+mod06.end()
